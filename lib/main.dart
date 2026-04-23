d4ab1e6d561dffd3cc7cceabbbae32a2417ddec9
FrancescoSireno <sirenofrancesco@gmail.com>
2026-04-23 19:56:01 -0300

    teste 2

--- lib/main.dart
+++ lib/main.dart
@@ -157,7 +157,7 @@ class HomeScreen extends StatelessWidget {
                 _Bullet('Decore'),
                 _Bullet('A realidade aumentada é isso e muito mais'),
               ],
-              const SizedBox(height: 14),
+              const SizedBox(height: 20),
               const Text(
                 'A DecorAR é uma empresa inovadora especializada em decoração de interiores com Realidade Aumentada (RA). '
                 'Nosso objetivo é transformar a forma como as pessoas planejam e visualizam seus ambientes, permitindo '
@@ -169,7 +169,7 @@ class HomeScreen extends StatelessWidget {
                 'para explorar modelos, conhecer inspirações e iniciar a jornada de decoração com praticidade e confiança.',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 34,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
                   height: 1.1,
                 ),
